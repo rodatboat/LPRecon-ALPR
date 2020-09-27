9913e70b20735c9490f6872a14528fa1016ccdc1
Ronaldo <ronaldo3055@gmail.com>
2020-09-27 03:22:55 -0400

    fetch fix

--- fetch.py
+++ fetch.py
@@ -5,12 +5,15 @@ load_dotenv()
 
 license_number = sys.argv[1] #LICENSE_NUMBER
 
-mongodblink = os.environ['DB_URL']
+pwd = os.environ['PASS']
+customers = os.environ['COLLECTION']
+
+mongodblink = f"mongodb+srv://admin:{pwd}@user-data.5mz9v.mongodb.net/{customers}?retryWrites=true&w=majority"
 
 cluster = MongoClient(mongodblink)
 collection = cluster['user-data']
 
-users = collection['customers']
+users = collection[customers]
 
 for user in users.find({"license":f"{license_number}"}):
     print(f"{user['name']} {user['license']}")
