368658604e58c9feb971b3a0a22a43900f0a623f
Manuel Toledo <mtole035@fiu.edu>
2020-09-27 03:24:08 -0400

    GUI WORKINGpip3 install flask

--- imgurpush.py
+++ imgurpush.py
@@ -1,8 +1,29 @@
 import pyimgur
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-CLIENT_ID = ""
-PATH = "./license_plates/4.jpg"
 
-im = pyimgur.Imgur(CLIENT_ID)
-uploaded_image = im.upload_image(PATH, title="LP Finder")
-print(uploaded_image.link)
+CLIENT_ID = os.getenv("TOKEN")
+# PATH = "./license_plates/4.jpg"
+
+# im = pyimgur.Imgur(CLIENT_ID)
+# uploaded_image = im.upload_image(PATH, title="LP Finder")
+# print(uploaded_image.link)
+
+
+
+def pushImage(image_Location):
+    print('pushing')
+    path = "./plate_copies/{}".format(image_Location)
+    im = pyimgur.Imgur(CLIENT_ID)
+    uploaded_image = im.upload_image(path, title="LP Finder")
+    print(os.path.splitext(os.path.basename(uploaded_image.link))[0])
+    sendToServer((os.path.splitext(os.path.basename(uploaded_image.link))[0]))
+
+
+def sendToServer(id):
+    url = f'http://34.75.252.241:3000/api/getLP/{id}'
+    r = requests.get(url)
+    print(r.text)
